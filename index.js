db6250c97cf5470935db16535992db5631c6de66
Nerajno <Nerajno@gmail.com>
2019-07-24 14:25:53 -0400

    ALEX RULES

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, action) {
+          if(Array.isArray(collection)){
+            for(let i = 0; i < collection.length; i++){
+              action(collection[i], i, collection)}
+          }else{
+            for(let key in collection){
+              action(collection[key], key, collection)}
+          }
+        },
 
+
+    map: function(collection, action) {
+        let mappedArr = [];
+          for(let i = 0; i < collection.length; i++){
+            mappedArray.push(action(collection[i], i, collection))
+          }
+          return mappedArr;     
     },
 
-    map: function() {
-
-    },
-
-    reduce: function() {
+    reduce: function(array, test, seed) {
+      each(array(function(e, i, a){
+        if(seed === undefined){
+          seed = array[0];
+        }else{
+          seed = test(seed, e, i)
+        }
+      }))
+      return seed
 
     },
 
@@ -21,7 +40,9 @@ const fi = (function() {
     },
 
 
-  }
+  //  }
+   
+  
 })()
 
 fi.libraryMethod()
